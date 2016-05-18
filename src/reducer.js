13359e2f3e69e9500cd3a1d0e2bf9846094b5e33
Eric McKay <eric@andcode.co>
2016-05-18 07:02:43 +0000

    reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,22 @@
-export function reducer(initialState, action) {
+import { Map } from 'immutable';
+
+const toggleItem = (currentState, id) => {
+    const items = currentState.get('items');
+    const itemIndex = items.findIndex( (el) => el.get('id') == id);
     
+    const oldItem = items.get(itemIndex);
+    const newItem = oldItem.set('complete', !oldItem.get('complete'));
+    const newItems = items.set(itemIndex, newItem);
+    
+    return currentState.merge({items: newItems});
+};
+
+export function reducer(currentState = new Map(), action) {
+    if(action !== undefined) {
+        switch(action.type) {
+            case 'TOGGLE_ITEM':
+                return toggleItem(currentState, action.id);
+        }
+    }
+    return currentState;
 }
--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -1,12 +1,31 @@
 import { expect } from 'chai';
-import { Map } from 'immutable';
+import { Map, fromJS } from 'immutable';
 
 import { reducer } from '../src/reducer.js';
-
-
 
 describe("reducer()", () => {
     it('returns empty Map if currentState is undefined', () => {
         expect(reducer(undefined, undefined)).to.eq(Map());
+    });
+    
+    describe('TOGGLE_ITEM', () => {
+        const currentState = fromJS({ 
+            items: [ 
+                { id: 3, complete: false }, 
+                { id: 2, complete: true } 
+            ]
+        });
+        const action = { type: "TOGGLE_ITEM", id: 3 };
+        
+        const nextState = reducer(currentState, action);
+        
+        it('changes complete for correct item', () => {
+            expect(nextState.getIn(['items', 0, 'complete'])).to.eq(true);
+        });
+        
+        it('does not change complete for other items', () => {
+            expect(nextState.getIn(['items', 1, 'complete'])).to.eq(true);
+        });
+        
     });
 });
